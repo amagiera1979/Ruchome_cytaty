ae094eb8bb55662491ae96bb0cbe4e1156191e39
Agnieszka MAGIERA <agnieszka_magiera@wp.pl>
2022-02-10 11:05:06 +0100

    Utworzenie pliku js i podpiecie, wypisanie tablicy z cytatami

---
+++ Script1.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+const quotes = [
+    { "quote": "To, ¿e milczê, nie znaczy, ¿e nie mam nic do powiedzenia.", "author": "Jonathan Carroll" },
+    { "quote": "Lepiej zaliczaæ siê do niektórych, ni¿ do wszystkich.", "author": "Andrzej Sapkowski" },
+    { "quote": "Czytanie ksi¹¿ek to najpiêkniejsza zabawa, jak¹ sobie ludzkoœæ wymyœli³a.", "author": "Wis³awa Szymborska " },
+    { "quote": "Ksi¹¿ki s¹ lustrem: widzisz w nich tylko to co, ju¿ masz w sobie.", "author": "Carlos Ruiz Zafón" },
+    { "quote": "Strach tnie g³êbiej ni¿ miecze.", "author": "George R.R. Martin" },
+    { "quote": "Je¿eli robisz to co ³atwe, Twoje ¿ycie bêdzie trudne. Jeœli robisz to co trudne, Twoje ¿ycie bêdzie ³atwe.", "author": "Les Brown" }
+];
+
